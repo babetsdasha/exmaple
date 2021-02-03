479bacb4094c1631521745ff2a5ef60228017017
Vasya <babetsdasha@gmail.com>
2021-02-03 21:20:14 +0200

    First change

---
+++ main.py
@@ -0,0 +1,22 @@
+x = """Aute pariatur cupidatat pariatur duis ut. Nulla cillum nostrud anim anim nulla et sunt fugiat aliquip ut magna fugiat cillum. Pariatur duis aliqua est commodo proident commodo. Elit nulla reprehenderit tempor anim sit in est qui esse. Incididunt reprehenderit sint et dolor. Do enim culpa cupidatat anim ex reprehenderit aliquip.
+
+Deserunt tempor tempor eiusmod esse quis magna sunt nisi. Consequat in et aliqua do ad. Sint pariatur cillum dolor non do id esse quis quis qui.
+
+Minim incididunt veniam duis duis ipsum exercitation cupidatat aliquip ipsum fugiat irure. Ullamco et incididunt reprehenderit duis esse. Cillum aliquip tempor occaecat non eiusmod aute consectetur quis magna.
+
+Anim et ullamco occaecat nostrud ad veniam velit in. Incididunt non aute nostrud commodo duis culpa sunt mollit qui velit incididunt non. Labore ullamco magna pariatur culpa aliqua minim velit proident et sint culpa deserunt. Quis laboris labore dolore do ad. Ex dolor ex est qui dolor ad consequat.
+
+Nulla in esse do pariatur quis magna eu nisi. Laborum aliqua quis quis cillum. Aliqua nostrud est nulla excepteur exercitation. Id quis ut consequat reprehenderit deserunt ea. Pariatur do ipsum non magna sint qui adipisicing deserunt.
+
+Aliquip ea sunt non fugiat. Excepteur reprehenderit commodo do aliquip commodo aliquip elit sunt minim. Adipisicing adipisicing fugiat anim aute nulla fugiat adipisicing. Anim sunt magna ullamco velit ullamco cupidatat sit amet consequat anim.
+
+Id commodo esse quis ullamco pariatur non culpa duis fugiat fugiat consectetur consequat nostrud. Qui in excepteur veniam tempor amet consequat. Nisi id officia laboris officia do laborum amet labore dolor Lorem.
+
+Velit aliqua velit laborum commodo esse ullamco. Amet officia pariatur elit occaecat do. Commodo velit velit consequat amet aute ipsum proident anim qui anim eiusmod non est. Cupidatat officia ad magna aliquip minim et.
+
+Nulla fugiat occaecat deserunt consectetur commodo eiusmod ex exercitation enim sit fugiat incididunt ex. Est qui laboris ut aliqua consequat tempor in culpa dolor aliquip. Ex ad do aliquip aute. Nulla consequat minim do sint in ut laborum nulla commodo.
+
+Ex adipisicing fugiat officia reprehenderit laborum est magna sit mollit dolor. Ipsum proident tempor officia nisi consectetur reprehenderit tempor sint pariatur. In ullamco officia officia in est do dolor pariatur occaecat ad minim. Reprehenderit nulla quis qui laboris nisi."""
+
+a = x.split()
+print(a)
