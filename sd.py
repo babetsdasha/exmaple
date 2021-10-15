4a9d92aecf6c52c82e1c338839ecda417d41d738
Vasyl Sk <greenzp93@gmail.com>
2021-10-15 23:01:47 +0300

    1 command commit

--- sd.py
+++ sd.py
@@ -1,9 +1,11 @@
+import test.test
+
+
 a = list(str(45654645345324502542452))
 
 while len(a) >= 2:
     x = 0
     for i in a:
         x += int(i)
-    a = list(str(x)) 
+    a = list(str(x))
 print(x)
-
